3f52b1cc0c51767ef86728ae83aa0e7f74aae363
JesseeGatimu <gakuojessee@gmail.com>
2026-09-04 07:10:37 +0300

    Cropped the numberplate

---
+++ crop_plate.py
@@ -0,0 +1,22 @@
+from ultralytics import YOLO
+import cv2
+model=YOLO("runs/detect/train-2/weights/best.pt")
+image=cv2.imread("gari.jpeg")
+if image is None:
+    print("Image not found")
+    exit()
+results=model(image)
+boxes=results[0].boxes
+for i, box in enumerate(boxes):
+    x1,y1,x2,y2=box.xyxy[0].tolist()
+    x1,y1,x2,y2=map(int,[x1,y1,x2,y2])
+    #crop the plate
+    plate_crop=image[y1:y2, x1:x2]
+    #save the cropped number plate
+    cv2.imwrite(f"plate_{i}.jpg",plate_crop)
+    print("Saved: plate_{i}.jpg")
+    #Display the cropped plate
+    cv2.imshow("Cropped Lincense plate",plate_crop)
+    cv2.waitKey(0)
+cv2.destroyAllWindow()
+
--- unseen.py
+++ unseen.py
@@ -1,13 +1,10 @@
-#Testing the model with some unseen images to see how it performs
 from ultralytics import YOLO
 import cv2
-model=YOLO(
-    "runs/detect/train-2/weights/best.pt"
-)
-img=cv2.imread("gari.jpeg")
-results=model(img)
-annonated_image=results[0].plot()
-print(results[0].boxes)
-cv2.imshow("Detected plate",annonated_image)
+model=YOLO("runs/detect/train-2/weights/best.pt")
+image=cv2.imread("two_cars.jpeg")
+result=model(image)
+annonated=result[0].plot()
+cv2.imshow("Detected plate",annonated)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+print(result[0].boxes)
