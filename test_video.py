3632b4b577f6600c23ba724e8ece5b2701296552
JesseeGatimu <gakuojessee@gmail.com>
2026-09-16 12:44:56 +0300

    Added YOLO to see how the model performs on videos

--- test_video.py
+++ test_video.py
@@ -1,8 +1,21 @@
 import cv2
+from ultralytics import YOLO
+model=YOLO("runs/detect/train-2/weights/best.pt")
 video=cv2.VideoCapture("car_video.mp4")
 while True:
     success, frame=video.read()
     if not success:
         break
-    print("Video read successfully .")
+    result=model(frame)
+    boxes=result[0].boxes
+    print("Number of plates detected :",len(boxes))
+    for i,box in enumerate(boxes):
+        x1,y1,x2,y2=box.xyxy[0].tolist()
+        x1,y1,x2,y2=map(int,[x1,y1,x2,y2])
+        print(
+            "plate",i,
+            "cordinated",
+            x1,y1,x2,y2
+        )
+
 video.release()
