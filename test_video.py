7a3e388d982d05d1c062350476bcd20686d2d0fb
JesseeGatimu <gakuojessee@gmail.com>
2026-09-18 06:59:41 +0300

    Added Easy OCR to detect the lincense plates

--- test_video.py
+++ test_video.py
@@ -1,10 +1,12 @@
 import cv2
 from ultralytics import YOLO
 import os
+import easyocr
 model=YOLO("runs/detect/train-2/weights/best.pt")
 video=cv2.VideoCapture("car_video.mp4")
 os.makedirs("video_plates",exist_ok=True)
 frame_number=0
+reader=easyocr.Reader(["en"])
 total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
 frame_interval = total_frames // 5
@@ -26,11 +28,10 @@ while True:
         x1,y1,x2,y2=box.xyxy[0].tolist()
         x1,y1,x2,y2=map(int,[x1,y1,x2,y2])
         plate_crop=frame[y1:y2,x1:x2]
-        print(
-            "plate",i,
-            "cordinated",
-            x1,y1,x2,y2
-        )
+        ocr_results=reader.readtext(plate_crop)
+        for bbox,text, ocr_confidence in ocr_results:
+            print("Lincense Plate ::",text)
+            print("OCR Confidence ::",ocr_confidence)
         crop_path = os.path.join(
             "video_plates",
             f"plate_{frame_number}_{i}.jpg"
